7a9435bd4ff548049ee01da7c20b283ee48061a7
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2019-08-10 16:02:41 -0300

    changed logging

--- linked_cto/spiders/cto_spider.py
+++ linked_cto/spiders/cto_spider.py
@@ -17,14 +17,14 @@ class CTOSpider(scrapy.Spider):
     name = "cto"
 
     def __init__(self, user,password):
-        self.log("inside __init__ method")
+        self.logger.info("inside __init__ method")
         self.driver = webdriver.Chrome('/home/paf/Downloads/chromedriver')
         self.driver.fullscreen_window()
         self.login(user, password)
         self.driver.get("https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22us%3A0%22%5D&keywords=Chief%20Transformation%20Officer&origin=FACETED_SEARCH")
 
     def start_requests(self):
-        self.log("inside start_requests method")
+        self.logger.info("inside start_requests method")
         urls = []
 	for page in xrange(1,101):
         	urls.append("https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22us%3A0%22%5D&keywords=Chief%20Transformation%20Officer&origin=FACETED_SEARCH&page="+str(page))
@@ -33,20 +33,20 @@ class CTOSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        self.log("inside parse method")
+        self.logger.info("inside parse method")
         url = response.url
-        self.log("response url: " + url )
+        self.logger.info("response url: " + url )
         self.driver.get(url)
-        self.log("response request url: " + response.request.url)
+        self.logger.info("response request url: " + response.request.url)
         self.scroll_down()
-        self.log("finished scrolling down")
+        self.logger.info("finished scrolling down")
         search_result = self.get_search_result_list()
         for result in search_result:
             self.process_result(result)
     
 
     def login(self, user, password):
-        self.log("starting login")
+        self.logger.info("starting login")
         self.driver.get("https://www.linkedin.com/login")
 
         self.driver.find_element_by_id("username").send_keys(user)
